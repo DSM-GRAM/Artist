843d3ad98e8fa79d7313f6ecd2cbbd5a6c199b98
PlanB <city7310@naver.com>
2017-09-07 15:50:33 +0900

    Enhanced logger

--- Server/main.py
+++ Server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api
 
 import logging
@@ -13,48 +13,55 @@ logger = None
 
 @app.before_first_request
 def before_first_request():
-    handler = RotatingFileHandler('server_log.log', maxBytes=100000, backupCount=5)
-    # handler.setLevel(logging.DEBUG if debug else logging.INFO)
+    def make_handler():
+        handler = RotatingFileHandler('server_log.log', maxBytes=100000, backupCount=5)
+        # handler.setLevel(logging.DEBUG if debug else logging.INFO)
+        formatter = logging.Formatter("[%(asctime)s] %(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        # 로그 포매팅
 
-    formatter = logging.Formatter("[%(asctime)s] %(levelname)s - %(message)s")
-    handler.setFormatter(formatter)
-    # 로그 포매팅
+        return handler
 
-    app.logger.addHandler(handler)
-    app.logger.setLevel(logging.INFO)
-    # 'info' 로그가 성공적으로 로그 파일에 들어갈 수 있도록
-    # 기본값은 WARNING(30)
+    def make_logger():
+        app.logger.addHandler(make_handler())
+        app.logger.setLevel(logging.INFO)
 
-    app.logger.info('Logger started')
+        return app.logger
+        # 기본값은 WARNING(30)
 
     global logger
-    logger = app.logger
+    logger = make_logger()
     # 로거 set
+
+    logger.info('-------- Logger started')
 
 
 @app.before_request
 def before_request():
-    pass
+    logger.info('Requested from {0} [ {1} {2} ]'.format(request.host, request.method, request.url))
+    logger.info('Request data : {0}'.format(request.form))
 
 
 @app.after_request
 def after_request(response):
     # flask.wrapper.Response 클래스의 인스턴스
+    logger.info('Response data : {0}'.format(response.data))
+    logger.info('Response status : {0}'.format(response.status))
 
     return response
 
 
 @app.teardown_request
 def teardown_request(exception):
-    pass
+    logger.info('Teardown request')
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
-    pass
+    logger.info('---- Teardown appcontext')
 
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
     return 'hello'
 
