09610771d829b2fd7e4b696144fb869759a12400
PlanB <city7310@naver.com>
2017-09-07 14:34:45 +0900

    Flask 기반 기본 서버 구성

--- Server/main.py
+++ Server/main.py
@@ -1,1 +1,16 @@
+from flask import Flask
+from flask_restful import Api
 
+app = Flask(__name__)
+api = Api(app)
+
+debug = True
+
+
+@app.route('/')
+def index():
+    return 'hello'
+
+
+if __name__ == '__main__':
+    app.run(debug=debug)
