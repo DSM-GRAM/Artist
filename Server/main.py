4884b4d6c2d2c6f22c8a54aef0d85c7c864c9a5c
PlanB <city7310@naver.com>
2017-09-07 14:52:42 +0900

    logger 문제 해결

--- Server/main.py
+++ Server/main.py
@@ -8,18 +8,28 @@ app = Flask(__name__)
 api = Api(app)
 
 debug = True
+logger = None
 
 
 @app.before_first_request
 def before_first_request():
     handler = RotatingFileHandler('server_log.log', maxBytes=100000, backupCount=5)
-    handler.setLevel(logging.DEBUG if debug else logging.INFO)
+    # handler.setLevel(logging.DEBUG if debug else logging.INFO)
 
     formatter = logging.Formatter("[%(asctime)s] %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
+    # 로그 포매팅
 
     app.logger.addHandler(handler)
+    app.logger.setLevel(logging.INFO)
+    # 'info' 로그가 성공적으로 로그 파일에 들어갈 수 있도록
+    # 기본값은 WARNING(30)
+
     app.logger.info('Logger started')
+
+    global logger
+    logger = app.logger
+    # 로거 set
 
 
 @app.before_request
