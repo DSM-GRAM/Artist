5fa9653c16c34e38e968e52b322810929a6352d0
PlanB <city7310@naver.com>
2017-09-07 20:15:29 +0900

    Add new score

--- Server/routes/api/user/user.py
+++ Server/routes/api/user/user.py
@@ -1,3 +1,14 @@
 from flask import request
 from flask_restful import Resource
 from database.models.user import *
+
+
+class Score(Resource):
+    def post(self):
+        phone = request.form['phone']
+        name = request.form['name']
+        affiliation = request.form['affiliation']
+        age = request.form['age']
+        score = request.form['score']
+
+        return '', 201 if add_new_score(phone, name, affiliation, age, score) else 204
