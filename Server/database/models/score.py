f9539c5641f9df7672f78c6f967d680524f08c48
PlanB <city7310@naver.com>
2017-09-07 18:58:00 +0900

    Add user.py, score.py

---
+++ Server/database/models/score.py
@@ -0,0 +1,13 @@
+from database.mongodb import db
+
+col = db.score
+
+
+def add_new_score(phone_number, score):
+    data = {
+
+    }
+
+
+def get_ranking():
+    pass
---
+++ Server/database/models/user.py
@@ -0,0 +1,15 @@
+from database.mongodb import db
+
+col = db.user
+
+
+def create_user(phone_number, name, affiliation, age):
+    data = {
+
+    }
+
+
+def delete_user(phone_number):
+    data = {
+
+    }
