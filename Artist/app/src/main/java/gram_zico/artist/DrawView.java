a5add5bbd201f86d8687a9eb1b30530a49c839b2
half-but <qudcks9191@naver.com>
2017-09-19 23:06:29 +0900

    set color width setting

--- Artist/app/src/main/java/gram_zico/artist/DrawView.java
+++ Artist/app/src/main/java/gram_zico/artist/DrawView.java
@@ -1,69 +1,77 @@
 package gram_zico.artist;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.LinkedList;
 
 
 /**
  * Created by dsm2017 on 2017-09-15.
  */
 
+
 public class DrawView extends View {
 
-    private Paint paint = new Paint();//그림 그리기 버튼
-    private Path path = new Path();
+    private LinkedList<DrawClass> linkedList = new LinkedList<>();
 
-    private int color = Color.BLACK;
-    private int width = 10;
+    private Paint paint = new Paint();
+    private Path path;
 
-    float y ;//그림 그리는 좌표 설정
-    float x ;
-
-    private Paint.Style style;
+    private Bitmap bitmap;
 
     public DrawView(Context context) {
         super(context);
     }
 
-    public DrawView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
     public void changeColor(int color){
-        this.color = color;
+        linkedList.add(new DrawClass(path, paint));
+        paint.setColor(color);
+        path = new Path();
     }
 
     public void changeWidth(int size){
-        this.width = size;
+        linkedList.add(new DrawClass(path, paint));
+        paint.setStrokeWidth(size);
+        path = new Path();
     }
 
-
-
-
     protected void onDraw(Canvas canvas) {
-        paint.setStrokeWidth(width);
-        paint.setAntiAlias(true);
-        paint.setColor(color);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setDither(true);
-        paint.setStrokeJoin(Paint.Join.ROUND);
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        canvas.drawPath(path, paint);
+        if (bitmap == null){
+            paint.setStrokeWidth(10);
+            paint.setColor(Color.BLACK);
+            paint.setStyle(Paint.Style.STROKE);
+            bitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
+        }
 
+        canvas.setBitmap(bitmap);
+        for(DrawClass drawClass : linkedList){
+            canvas.drawPath(drawClass.path, drawClass.paint);
+        }
+
+        canvas.drawPath(path, paint);
+    }
+
+    class DrawClass{
+        private Path path;
+        private Paint paint;
+
+        public DrawClass(Path path, Paint paint) {
+            this.path = path;
+            this.paint = paint;
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
-        x = event.getX();
-        y = event.getY();
+        float x = event.getX();
+        float y = event.getY();
 
         switch(event.getAction()) {
             case MotionEvent.ACTION_DOWN:
