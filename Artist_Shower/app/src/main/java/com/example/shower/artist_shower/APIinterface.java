750c33db2582a1881dfe0207bf883b82da5565e2
Jeong-minji <alswl4152@naver.com>
2017-09-19 22:00:55 +0900

    artist_shower

---
+++ Artist_Shower/app/src/main/java/com/example/shower/artist_shower/APIinterface.java
@@ -0,0 +1,25 @@
+package com.example.shower.artist_shower;
+
+import com.google.gson.JsonObject;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * Created by dsm2016 on 2017-09-19.
+ */
+
+public interface APIinterface {
+
+    @FormUrlEncoded
+    @POST("/new-sample")
+    Call<Void> callSample(@Field("category") int category);
+
+    @GET("/sample/{_id}")
+    Call<JsonObject> getSample(@Path("id") int sample_id);
+
+}
---
+++ Artist_Shower/app/src/main/java/com/example/shower/artist_shower/ShowSample.java
@@ -0,0 +1,25 @@
+package com.example.shower.artist_shower;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ShowSample extends AppCompatActivity {
+
+    private APIinterface apIinterface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_sample);
+
+//        RetrofitClass.getClient().
+    }
+}
